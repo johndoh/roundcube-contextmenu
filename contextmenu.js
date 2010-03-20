a52f27e6a7f8c6eb2d5966fdba0dd5c02c82cfc0
PhilW <roundcube@tehinterweb.co.uk>
2010-03-20 17:20:50 +0000

    move collapsed threads

--- contextmenu.js
+++ contextmenu.js
@@ -86,9 +86,39 @@ function rcm_contextmenu_init(row) {
 					if (rcmail.env.rcm_destfolder == rcmail.env.mailbox)
 						return;
 
+					// also select childs of (collapsed) threads for dragging
+					if (rcmail.message_list.rows[rcmail.env.uid].has_children) {
+						rcmail.message_list.select_row(rcmail.env.uid, CONTROL_KEY);
+ 						rcmail.env.uid = null;
+
+ 						var selection = $.merge([], rcmail.message_list.selection);
+ 						var depth, row, uid, r;
+ 						for (var n=0; n < selection.length; n++) {
+ 							uid = selection[n];
+ 							if (rcmail.message_list.rows[uid].has_children /*&& !this.rows[uid].expanded*/) {
+ 								depth = rcmail.message_list.rows[uid].depth;
+ 								row = rcmail.message_list.rows[uid].obj.nextSibling;
+
+ 								while (row) {
+ 									if (row.nodeType == 1) {
+ 										if ((r = rcmail.message_list.rows[row.uid])) {
+ 											if (!r.depth || r.depth <= depth)
+ 												break;
+
+ 											if (!rcmail.message_list.in_selection(r.uid))
+ 												rcmail.message_list.select_row(r.uid, CONTROL_KEY);
+ 										}
+ 									}
+
+ 									row = row.nextSibling;
+ 								}
+ 							}
+ 						}
+					}
+
 					rcmail.command(command, rcmail.env.rcm_destfolder, $(el));
 
-					if (rcmail.message_list.selection.length <= 1)
+					if (rcmail.message_list.selection.length <= 1 && rcmail.env.uid)
 						rcmail.message_list.remove_row(rcmail.env.uid, false);
 
 					rcmail.env.rcm_destfolder = null;
