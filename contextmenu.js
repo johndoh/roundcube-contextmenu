804129c8b1cb3cc3f011fdb317003e8a4a35bf1a
PhilW <roundcube@tehinterweb.co.uk>
2010-03-20 17:23:56 +0000

    fix code formatting

--- contextmenu.js
+++ contextmenu.js
@@ -89,31 +89,31 @@ function rcm_contextmenu_init(row) {
 					// also select childs of (collapsed) threads for dragging
 					if (rcmail.message_list.rows[rcmail.env.uid].has_children) {
 						rcmail.message_list.select_row(rcmail.env.uid, CONTROL_KEY);
- 						rcmail.env.uid = null;
+						rcmail.env.uid = null;
 
- 						var selection = $.merge([], rcmail.message_list.selection);
- 						var depth, row, uid, r;
- 						for (var n=0; n < selection.length; n++) {
- 							uid = selection[n];
- 							if (rcmail.message_list.rows[uid].has_children /*&& !this.rows[uid].expanded*/) {
- 								depth = rcmail.message_list.rows[uid].depth;
- 								row = rcmail.message_list.rows[uid].obj.nextSibling;
+						var selection = $.merge([], rcmail.message_list.selection);
+						var depth, row, uid, r;
+						for (var n=0; n < selection.length; n++) {
+							uid = selection[n];
+							if (rcmail.message_list.rows[uid].has_children /*&& !this.rows[uid].expanded*/) {
+								depth = rcmail.message_list.rows[uid].depth;
+								row = rcmail.message_list.rows[uid].obj.nextSibling;
 
- 								while (row) {
- 									if (row.nodeType == 1) {
- 										if ((r = rcmail.message_list.rows[row.uid])) {
- 											if (!r.depth || r.depth <= depth)
- 												break;
+								while (row) {
+									if (row.nodeType == 1) {
+										if ((r = rcmail.message_list.rows[row.uid])) {
+											if (!r.depth || r.depth <= depth)
+												break;
 
- 											if (!rcmail.message_list.in_selection(r.uid))
- 												rcmail.message_list.select_row(r.uid, CONTROL_KEY);
- 										}
- 									}
+											if (!rcmail.message_list.in_selection(r.uid))
+												rcmail.message_list.select_row(r.uid, CONTROL_KEY);
+										}
+									}
 
- 									row = row.nextSibling;
- 								}
- 							}
- 						}
+									row = row.nextSibling;
+								}
+							}
+						}
 					}
 
 					rcmail.command(command, rcmail.env.rcm_destfolder, $(el));
