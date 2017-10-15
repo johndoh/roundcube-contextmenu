2f590257d5a7e12dd3e538d105f2ffa4ddea8fef
PhilW <roundcube@tehinterweb.co.uk>
2017-10-15 13:17:18 +0100

    add elastic skin support

---
+++ skins/elastic/functions.js
@@ -0,0 +1,116 @@
+/**
+ * ContextMenu plugin script
+ *
+ * @licstart  The following is the entire license notice for the
+ * JavaScript code in this file.
+ *
+ * Copyright (C) 2017 Philip Weir
+ *
+ * The JavaScript code in this page is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this file.
+ */
+
+function rcm_add_menu_text(p) {
+    if (matches = $(p.item).children('a').attr('class').match(/\b(addto|addcc|addbcc)\b/)) {
+        $(p.item).children('a').children('span').text($('#layout > .sidebar > div.footer a.' + matches[1]).attr('title'));
+    }
+}
+
+function rcm_reorder_contact_menu(p) {
+    // put export link last
+    var ul = $(p.ref.container).find('ul:first');
+    $(p.ref.container).find('a.export').parent('li').appendTo(ul);
+
+    // put assign group link before remove
+    $(p.ref.container).find('a.assigngroup').parent('li').insertBefore($(p.ref.container).find('a.removegroup').parent('li'));
+}
+
+function rcm_submenu_toggle(p) {
+    if (p.id.match(/^(#[^\s]+(\s>\s\.[a-z]+\s>)?(\sdiv\.footer)?)/)) {
+        var source_id = RegExp.$1;
+
+        // special handling for additional options in folders and contacts menus
+        if (source_id == '#rcmFolderMenu' || source_id == '#rcmAddressBookMenu') {
+            source_id = $(source_id).parent();
+        }
+
+        // make sure its a real submenu and not the toolbar on a small screen
+        if ($(source_id).is('div')) {
+            $(source_id)[(p.show) ? 'show' : 'hide']();
+        }
+    }
+}
+
+$(document).ready(function() {
+    if (window.rcmail) {
+        rcmail.context_menu_settings = $.extend(true, rcmail.context_menu_settings, {
+            popup_attrib: 'data-popup',
+            popup_func: "rcm_popup_wrapper('$2');",
+            popup_pattern: /rcm_popup_wrapper\(\'([^\']+)\'|^([a-z0-9\-]+)$/i,
+            classes: { button_ignore: '', modal_overlay: 'popover-overlay' },
+            menu_defaults: {
+                modal: true,
+                classes: {
+                    div: rcmail.context_menu_settings.classes.container + ' popover',
+                    ul: 'toolbarmenu toolbar listing',
+                    a: 'button rcmbutton',
+                    sub_button_a: 'rcmsubbutton',
+                    sub_button_span: null
+                }
+            },
+            menu_events: {
+                '+init': function(p) {
+                    level = $('div.contextmenu:visible').length;
+
+                    var label = level >= 1 ? 'back' : 'close',
+                        title = rcmail.gettext(label),
+                        class_name = 'button icon ' + (label == 'back' ? 'back' : 'cancel');
+
+                    var header = $('<h3>').addClass('popover-header')
+                        .append($('<a>').attr('class', class_name).text(title))
+                        .click(function(e) {
+                            rcm_hide_menu(e, level >= 1);
+                        });
+
+                    $(p.ref.container).prepend(header).children('ul').wrap($('<div>').addClass('popover-body'));
+                },
+                '+beforeactivate': function(p) {
+                    // force toolbar display on small screens while the contextmenu renders
+                    if (!$('#layout > .content').is(':visible'))
+                        $('#layout > .content').addClass('contextmenu_content');
+
+                    // @TODO: Is this needed?
+                    // change default message list contextmenu to look like normal rc menu when on small screen
+                    //$('a.rcm_elem_markmessagemenulink,a.rcm_elem_messagemenulink')[($('html').hasClass('layout-phone') || $('html').hasClass('layout-small')) ? 'removeClass' : 'addClass']('rcmsubbutton');
+                },
+                '+afteractivate': function(p) {
+                    $('#layout > .content').removeClass('contextmenu_content');
+                },
+                'submenu_toggle': function(p) { rcm_submenu_toggle(p); }
+            }
+        });
+
+        if (rcmail.env.task == 'mail' && rcmail.env.action == '') {
+            rcmail.addEventListener('insertrow', function(props) { rcm_listmenu_init(props.row.id, {'menu_name': 'messagelist', 'menu_source': '#toolbar-menu > li'}); } );
+            rcmail.add_onload("rcm_foldermenu_init('#mailboxlist li', {'menu_source': ['#rcmFolderMenu', '#mailboxoptions-menu > ul > li']})");
+        }
+        else if (rcmail.env.task == 'mail' && rcmail.env.action == 'compose') {
+            rcmail.addEventListener('insertrow', function(props) { rcm_listmenu_init(props.row.id, {'menu_name': 'composeto', 'menu_source': '#layout > .sidebar > div.footer', 'list_object': 'contact_list'}, {
+                'insertitem': function(p) { rcm_add_menu_text(p); }
+            }); } );
+        }
+        else if (rcmail.env.task == 'addressbook' && rcmail.env.action == '') {
+            rcmail.addEventListener('insertrow', function(props) { rcm_listmenu_init(props.row.id, {'menu_name': 'contactlist', 'menu_source': ['#toolbar-menu > li', '#rcmAddressBookMenu'], 'list_object': 'contact_list'}, {
+                'init': function(p) { rcm_reorder_contact_menu(p); }
+            }); } );
+            rcmail.add_onload("rcm_abookmenu_init('#directorylist li, #savedsearchlist li', {'menu_source': ['#layout > .sidebar > div.footer a.create', '#groupoptions-menu > ul > li']})");
+            rcmail.addEventListener('group_insert', function(props) { rcm_abookmenu_init(props.li, {'menu_source': ['#layout > .sidebar > div.footer a.create', '#groupoptions-menu > ul > li']}); } );
+            rcmail.addEventListener('abook_search_insert', function(props) { rcm_abookmenu_init(rcmail.savedsearchlist.get_item('S' + props.id), {'menu_source': ['#layout > .sidebar > div.footer a.create', '#groupoptions-menu > ul > li']}); } );
+        }
+    }
+});
