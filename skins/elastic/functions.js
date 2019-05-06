fc9229087f3c048928376b452967c25a542aa8bd
PhilW <roundcube@tehinterweb.co.uk>
2019-05-06 14:54:14 +0100

    Quick fix for #114

--- skins/elastic/functions.js
+++ skins/elastic/functions.js
@@ -103,6 +103,12 @@ $(document).ready(function() {
         // Remove any tooltips added to menu elements by Elastic UI JS
         rcmail.addEventListener('enable-command', function() { $('div.contextmenu').find('a.rcmbutton').removeAttr('title'); });
 
+        // Hack to fix menu hiding when switching between BS popovers and JQUI dialogs (#114)
+        // TODO: find a more general solution
+        rcmail.addEventListener('afterqrcode', function(p) {
+            rcmail.contextmenu.hide_all(p.originalEvent, false, true);
+        });
+
         if (rcmail.env.task == 'mail' && rcmail.env.action == '') {
             rcmail.addEventListener('insertrow', function(props) { rcmail.contextmenu.init_list(props.row.id, {'menu_name': 'messagelist', 'menu_source': '#toolbar-menu > li'}); } );
             rcmail.add_onload("rcmail.contextmenu.init_folder('#mailboxlist li', {'menu_source': ['#rcmfolder-menu > ul', '#mailboxoptions-menu > ul > li']})");
